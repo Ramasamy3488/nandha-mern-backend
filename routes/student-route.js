bec35356052092a4ad0f7284f6b8e42334c630db
Ramasamy3488 <91617803+Ramasamy3488@users.noreply.github.com>
2025-10-15 20:24:28 +0530

    Update student-route.js

--- routes/student-route.js
+++ routes/student-route.js
@@ -3,9 +3,10 @@ const router = require('express').Router();
 const studentController = require('../controllers/student-controller');
 
 router.get('/readAllStudents', studentController.readAllStudents);
-router.get('/readAStudent', studentController.readAStudent);
+router.post('/readAStudent', studentController.readAStudent);
 router.post('/addAStudent', studentController.addAStudent);
 router.put('/updateAStudent', studentController.updateAStudent);
 router.delete('/deleteAStudent', studentController.deleteAStudent);
 
 module.exports = router;
+
